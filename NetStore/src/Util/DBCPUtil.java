e0dcc7583313c0e19d9083c6948cfd908f267722
wudi <mailswd@sina.com>
2018-11-26 22:56:36 +0800

    数据库连接，获取数据源，dao层数据存取

---
+++ NetStore/src/Util/DBCPUtil.java
@@ -0,0 +1,38 @@
+package Util;
+
+import org.apache.commons.dbcp.BasicDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBCPUtil {
+
+    private static DataSource dataSource;
+    static {
+        try {
+            InputStream in = DBCPUtil.class.getClassLoader().getResourceAsStream("dbcpconfig.properties");
+            Properties props = new Properties();
+            props.load(in);
+            dataSource = BasicDataSourceFactory.createDataSource(props);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static DataSource getDataSource(){
+        return dataSource;
+    }
+
+    public static Connection getConnection(){
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- NetStore/src/dao/impl/CategoryDaoImpl.java
+++ NetStore/src/dao/impl/CategoryDaoImpl.java
@@ -1,6 +1,43 @@
 package dao.impl;
 
+import Util.DBCPUtil;
 import dao.CategoryDao;
+import domain.Category;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class CategoryDaoImpl implements CategoryDao {
+
+    private QueryRunner qr = new QueryRunner(DBCPUtil.getDataSource());
+
+    @Override
+    public void save(Category c) {
+        try {
+            qr.update("insert into categorys (id, name, description) values (?,?,?)",c.getId(),c.getName(),c.getDescription());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Category> findAll() {
+        try {
+            return qr.query("select * from categorys",new BeanListHandler<Category>(Category.class));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Category findById(String categoryId) {
+        try {
+            return qr.query("select * from categorys where id = ?",new BeanHandler<Category>(Category.class),categoryId);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }

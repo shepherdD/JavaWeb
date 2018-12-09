dea4b29d92662ca5248c9cc7f2894e6d6128225b
wudi <mailswd@sina.com>
2018-12-09 10:15:30 +0800

    删除注释

--- NetStore/src/web/Controller/ManageServlet.java
+++ NetStore/src/web/Controller/ManageServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@WebServlet(name = "ManageServlet",urlPatterns = "/manage/ManageServlet")
 public class ManageServlet extends HttpServlet {
 
     private BusinessService s = new BusinessServiceImpl();
